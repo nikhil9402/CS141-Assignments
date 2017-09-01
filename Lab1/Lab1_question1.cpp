0fd3d32da00b1d697cf1812c95ba9835c41b13d7
Ayush Singh <ayush.singh@niser.ac.in>
2017-09-01 21:48:18 +0530

    Added Lab1 and Lab2 assignments

---
+++ Lab1/Lab1_question1.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int I;
+	char C;
+	float F;
+	bool B;
+	long L;
+	double D;
+
+	cout << "enter int: ";
+	cin >> I;
+
+	cout << "enter char: ";
+	cin >> C;
+
+	cout << "enter float: " ;
+	cin >> F;
+
+	cout << "enter bool: ";
+	cin >> B;
+
+	cout << "enter long: ";
+	cin >> L;
+
+	cout << "enter double: ";
+	cin >> D;
+
+	cout << I << endl;
+	cout << C << endl;
+	cout << F << endl;
+	cout << B << endl;
+	cout << L << endl;
+	cout << D << endl;
+
+	return 0;
+}
+
---
+++ Lab1/Lab1_question3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	float a,b;
+	cout << "Enter any two nos. a and b\n";
+	cin >>a>>b;
+
+	cout << "a+b=" << a+b;
+	cout << "\na-b="<<a-b;
+	cout << "\na*b="<<a*b;
+	cout << "\na/b=\n"<<a/b;
+	
+	return 0;
+}
